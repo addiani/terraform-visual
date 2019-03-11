5ffbef28923245528e58b45c2f77156a3e8a7fe2
addiani <45836684+addiani@users.noreply.github.com>
2019-03-11 17:21:14 -0500

    Delete autoscalin.tf

--- autoscalin.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_autoscaling_group" "dev" {
-    name = "dev"
-    lunch_configuration = "${aws_lunch_configuration.dev.name}"
-    min_size = "${var.min_size}"toscaling_group
-    max_size = "${var.max_size}"
-    availability_zones = ["eu-west-1b", "eu-west-1a", "eu-west-1c"]
-    vpc_zone_identifier = ["${aws_subnet.dev1.id}", "${aws_subnet.dev2.id"]
-}   
